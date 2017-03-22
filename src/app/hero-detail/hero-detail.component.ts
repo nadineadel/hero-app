fe5182423e8be72a9df477b87a1333f2c86b093b
nadine <nadineadelzayed@gmail.com>
2017-03-22 13:54:42 +0200

    seperate hero class to be reusable in any component and import it in components

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from './hero';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -8,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeroDetailComponent implements OnInit {
 
   constructor() { }
-
+  hero: Hero;
   ngOnInit() {
   }
 
