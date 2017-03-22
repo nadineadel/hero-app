09c6fb45c941ccb65c096fabee8dbde6d24cd976
nadine <nadineadelzayed@gmail.com>
2017-03-22 13:59:05 +0200

    used property binding and input property to bind hero to selected hero

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 import { Hero } from '../hero';
 
@@ -10,7 +10,7 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
 
   constructor() { }
-  hero: Hero;
+  @Input() hero: Hero;
   ngOnInit() {
   }
 
