cbe0e5798cc7d0f627116b841cdfbbd53f930d4e
nadine <nadineadelzayed@gmail.com>
2017-03-22 16:13:23 +0200

    generate hereos component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  providers: [HeroService]
+})
+export class HeroesComponent implements OnInit {
+  constructor(private heroService: HeroService) {
+  }
+
+  title = 'Tour of Heroes';
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
+
