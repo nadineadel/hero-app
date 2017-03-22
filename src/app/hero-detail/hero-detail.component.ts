798040c02f834c8c8d22dcb2265bb0282fdbf8b6
nadine <nadineadelzayed@gmail.com>
2017-03-22 17:55:43 +0200

    pass data to hero details comonent

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit, Input} from '@angular/core';
 
 import { Hero } from '../hero';
+import {HeroService} from '../hero.service';
+import {ActivatedRoute, Params} from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,9 +14,21 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {
+  }
   @Input() hero: Hero;
   ngOnInit() {
+    this.route.params.switchMap((params: Params) =>
+    this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
   }
+  goBack(): void {
+    this.location.back();
+  }
+
 
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,10 +8,11 @@ export class HeroService {
 
   constructor() {
   }
-
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
   }
-
-
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
+  }
 }
