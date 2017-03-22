dabb68e4925cf81e916e3465a3980bdb280dbb54
nadine <nadineadelzayed@gmail.com>
2017-03-22 16:23:57 +0200

    seperate heroes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 
-import {Hero} from './hero';
 import {HeroService} from './hero.service'
 
 @Component({
@@ -10,22 +9,11 @@ import {HeroService} from './hero.service'
   providers: [HeroService]
 })
 export class AppComponent implements OnInit {
-  constructor(private heroService: HeroService) {
+  constructor() {
   }
 
   title = 'Tour of Heroes';
-  selectedHero: Hero;
-  heroes: Hero[];
 
   ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import {AppComponent} from './app.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+import {HeroesComponent} from './heroes/heroes.component';
+import {HeroService} from "./hero.service";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HeroesComponent } from './heroes/heroes.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
